2ad31966a659ea6bf1c8d019eb1cdbbe79118492
rohangupta544 <rohansahu1234ssm@gmail.com>
2026-08-29 17:19:12 +0530

    feat: initialize express app with custom utilities, middleware, and database connectivity logic

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 //require('dotenv').config({path:'./env'});
 import "dotenv/config";
 
-
-import express from 'express';
 import connectDB from './db/index.js';
-const app=express()
 
-connectDB();
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 ,()=>{
+        console.log(`server is Running at PORT: ${process.env.PORT} `)
+    })
+})
+.catch((err)=>{
+    console.log(`MOngoDB Connection id Failed !!!!!!!! ${err}`)
+})
+
